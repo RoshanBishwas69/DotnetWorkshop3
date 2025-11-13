24ec85627f7c527d89fd33f021e6f6a0472780e8
CURDMaster24 <bishwasroshan11@gmail.com>
2025-11-13 08:29:30 +0545

    Task 2 completed successfully

---
+++ Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 2: NullOperations
+            NullOperations nullOps = new NullOperations();
+            nullOps.PerformNullChecks();
+        }
+    }
+}
